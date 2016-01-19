07688a5e1043a47d3b7b9cec825f32c59f33bc8e
Michael Freudenthaler <mf@vivid-planet.com>
2016-01-19 10:58:58 +0100

    Add a demo user

--- setup/setup.sql
+++ setup/setup.sql
@@ -9,3 +9,6 @@ INSERT INTO `kwc_paragraphs` (`id`, `component_id`, `pos`, `visible`, `component
 
 INSERT INTO `kwf_pages` (`id`, `pos`, `parent_id`, `is_home`, `filename`, `name`, `visible`, `hide`, `component`, `custom_filename`) VALUES
 (1, 1, 'root-main', 1, 'home', 'Home', 1, 0, 'paragraphs', 0);
+
+INSERT INTO `kwf_users` (`id`, `role`, `language`, `email`, `password`, `password_salt`, `gender`, `title`, `firstname`, `lastname`, `created`, `deleted`, `logins`, `last_login`) VALUES
+(9, 'admin', 'en', 'demo@koala-framework.org', 'b2c5ae6bb7bec6021e3224f316d8a0c0', '684e86989d', 'male', '', 'Koala', 'Framework', '2011-10-25 10:06:07', 0, 0, '2011-10-25 10:50:59');
